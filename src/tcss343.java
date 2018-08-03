2b42733cd1061f397caea268f3e9b46292b7dcc5
Josh Atherton <joshua9@uw.edu>
2018-08-03 15:52:09 -0700

    added tcss343 which will hold the implemented algorithms

---
+++ src/tcss343.java
@@ -0,0 +1,47 @@
+/*
+TCSS - 343 | Summer 2018
+Homework 4
+Joshua Atherton | Armoni Atherton
+ */
+
+/**
+ * Homework for solves the coin change problem using 3 different algorithm approaches
+ */
+public class tcss343 {
+
+    //3.1 Brute force
+    /**
+     * Design a brute force solution to solve this problem.
+     * You need to print the cheapest solution, as well as the sequence.
+     */
+    public void bruteForce() {
+
+    }
+
+
+    //3.2 Divide and conquer
+    /**
+     *Express the problem with a purely divide-and-conquer approach.
+     * Implement a recursive algorithm for the problem.
+     * Be sure to consider all sub-instances needed to compute a solution
+     * to the full input instance in the self-reduction, especially if it contains
+     * overlaps. As before, you need to print the solution, as well as the sequence.
+     */
+    public void divide() {
+
+    }
+
+
+    //3.3 Dynamic programming
+    /**
+     *Design a dynamic programming table for this problem. How is the table initial- ized?
+     * In what order will the table be filled? How would you use the table to
+     * find the cheapest sequence of canoe rentals from post 1 to post n? Implement
+     * the corresponding dynamic programming solution.
+     */
+    public void dynamic() {
+
+    }
+
+
+}
