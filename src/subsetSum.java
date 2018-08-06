f4fef9511579ee0158af5472b5baed27ef363a0a
Josh Atherton <joshua9@uw.edu>
2018-08-06 14:11:32 -0700

    subset sum problem creates dynamic array

---
+++ src/subsetSum.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+
+public class subsetSum {
+
+    /**
+     *
+     * @param s list to search for subset sum
+     * @param t target to find
+     * @return the dynamic array
+     */
+    public int[][] has_subSet(int s[], int t) {
+        int n = s.length;
+        int A[][] = new int[n][t + 1];
+        for (int i = 0; i < n; i++) {
+            A[i][0] = 1;
+        }
+        for (int j = 0; j <= t; j++) {
+            if (s[1] == 1) {
+                A[1][j] = 1;
+            } else {
+                A[1][j] = 0;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= t; j++) {
+                if (j >= s[i]) {
+                    if (A[i-1][j] == 1 || A[i-1][j-s[i]] == 1) {
+                        A[i][j] = 1;
+                    } else {
+                        A[i][j] = 0;
+                    }
+                } else {
+                    A[i][j] = A[i-1][j];
+                }
+            }
+        }
+
+        String found = (A[n-1][t] == 1) ? "true" : "true";
+        System.out.println(found);
+
+        return A;
+    }
+
+    /**
+     *
+     * @param list dynamic solution array to search
+     * @param t target sum
+     * @return if found or not
+     */
+    public boolean subsetSumRecover(int list[][], int t) {
+        boolean found = false;
+
+
+        return found;
+    }
+
+    public static void main(String[] args) {
+        subsetSum sub = new subsetSum();
+        int list[] = {2, 3, 6, 1, 4};
+        int target = 8;
+        int dynamic[][];
+        dynamic = sub.has_subSet(list, target);
+
+
+
+        for (int i = 0; i<dynamic.length; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j<dynamic[i].length; j++) {
+                System.out.print(dynamic[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println(']');
+        }
+
+
+
+
+
+    }
+}
+
+
