9bbb23e5b2e564461603c7bfc804653fe5250307
Josh Atherton <joshua9@uw.edu>
2018-08-10 17:44:27 -0700

    divide: gets the minimum cost but still need to get optimal path

--- src/divideConquer.java
+++ src/divideConquer.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
 public class divideConquer {
+    Stack<Integer> s = new Stack<>(); //TODO: delete after working
+
     public static void main(String[] args) {
         divideConquer dc = new divideConquer();
         int [][] matrix = {
@@ -14,22 +21,39 @@ public class divideConquer {
                 {-1, -1, -1, 0, 3},
                 {-1, -1, -1, -1, 0}};
 
-        dc.divide(matrix);
+        System.out.println(dc.divide(matrix, 0, matrix.length - 1));
 
     }
 
+    /**
+     * Gets the minimum cost but still need to get the optimal path
+     * @param A
+     * @param start
+     * @param end
+     * @return
+     */
+    public int divide(int[][] A, int start, int end) {
+        if (start == end) {
+            s.push(end);
+            System.out.println("return: " + s);
+            s.pop();
+            return 0;
+        }
+        s.push(start);
+        ArrayList<Integer> costs = new ArrayList<>();
+        for (int i = start + 1; i <= end; i++) {
 
-    public void divide(int[][] A) {
-        int[] result = null;
-        int[] path = null;
+            costs.add(A[start][i] + divide(A, i, end));
+        }
 
-        int rows = A.length;
-        int cols = A[0].length;
-        int[][] M = new int[rows][cols];
+        int minCost = Collections.min(costs);
 
-
-
-
+        System.out.printf("start: %d, End: %d, Costs: ", start, end);
+        System.out.println(costs);
+//        System.out.println(" before return " + s);
+//        s.pop();
+        System.out.println("stack: " + s);
+        return minCost;
     }
 
 }
