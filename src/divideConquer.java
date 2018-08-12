0935b122a8e2f23c62cd63419548aa5ad5aa7a0b
Josh Atherton <joshua9@uw.edu>
2018-08-11 19:46:38 -0700

    divide: gets the minimum cost and all paths possible, need to link to costs

--- src/divideConquer.java
+++ src/divideConquer.java
@@ -3,8 +3,26 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Stack;
 
+
 public class divideConquer {
-    Stack<Integer> s = new Stack<>(); //TODO: delete after working
+    static Stack<Integer> s = new Stack<>(); //TODO: delete after working
+    static ArrayList<pathCost> pathCostsList = new ArrayList<>();
+
+    public class pathCost {
+        private int[] path;
+        private int cost;
+        public pathCost(int[] path, int cost) {
+            this.path = path;
+            this.cost = cost;
+        }
+        public void incrementCost(int cost) {
+            this.cost += cost;
+        }
+        public String toString() {
+            return "Cost of path: " + Arrays.toString(path) + " = " + cost;
+        }
+    }
+
 
     public static void main(String[] args) {
         divideConquer dc = new divideConquer();
@@ -21,8 +39,23 @@ public class divideConquer {
                 {-1, -1, -1, 0, 3},
                 {-1, -1, -1, -1, 0}};
 
-        System.out.println(dc.divide(matrix, 0, matrix.length - 1));
+        System.out.println(dc.divide(matrix, 0, matrix.length - 1) + "\n");
 
+        for (pathCost pc : pathCostsList) {
+            System.out.println(pc);
+        }
+//        s.push(5); s.push(8);
+//        System.out.println(Arrays.toString(dc.stackToArray(s)));
+
+    }
+
+    public int[] stackToArray(Stack<Integer> s) {
+        int[] result = new int[s.size()];
+        Stack<Integer> temp = (Stack<Integer>)s.clone();
+        for(int i = s.size() - 1; i >= 0; i--) {
+            result[i] = temp.pop();
+        }
+        return result;
     }
 
     /**
@@ -36,23 +69,26 @@ public class divideConquer {
         if (start == end) {
             s.push(end);
             System.out.println("return: " + s);
-            s.pop();
+
+            int[] result = stackToArray(s);
+            pathCostsList.add(new pathCost(result, 5)); // how to get the costs?
+//            for (int i = 1; i <= end - 1; i++)
+//                s.pop();
+            s.pop(); s.pop();
             return 0;
         }
+
         s.push(start);
         ArrayList<Integer> costs = new ArrayList<>();
         for (int i = start + 1; i <= end; i++) {
-
             costs.add(A[start][i] + divide(A, i, end));
         }
 
         int minCost = Collections.min(costs);
 
-        System.out.printf("start: %d, End: %d, Costs: ", start, end);
-        System.out.println(costs);
-//        System.out.println(" before return " + s);
-//        s.pop();
-        System.out.println("stack: " + s);
+//        System.out.println("stack: " + s);
+        System.out.println("\tcosts: " + costs);
+//        System.out.printf("\treturnCost- s%d e%d = %d \n", start, end, minCost);
         return minCost;
     }
 
